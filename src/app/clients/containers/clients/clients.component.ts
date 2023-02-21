0d3df00be219db132eac2f91f1d35da300c8d146
Felipe Cajueiro <fferme@gmail.com>
2023-02-20 23:10:00 -0300

    Refatoração para uso de spinner Bootstrap

--- src/app/clients/containers/clients/clients.component.ts
+++ src/app/clients/containers/clients/clients.component.ts
@@ -1,7 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, first, of } from 'rxjs';
+import { catchError, delay, first, of } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { Client } from '../../model/client';
 import { ClientsService } from '../../services/clients.service';
@@ -27,6 +27,7 @@ export class ClientsComponent {
     this.clients$ = this.clientsService.list()
     .pipe(
       first(),
+      delay(3000),
       catchError(error => {
         this.onError('Erro ao carregar cursos.');
         return of([])
@@ -35,6 +36,7 @@ export class ClientsComponent {
   }
 
   onError(errorMsg: string) {
+
   }
 
   onBack() {
@@ -50,5 +52,24 @@ export class ClientsComponent {
   }
 
   onDelete(client: Client) {
+  //   const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+  //     data: 'Tem certeza que deseja remover esse curso?',
+  //   });
+
+  //   dialogRef.afterClosed().subscribe((result: boolean) => {
+  //     if (result) {
+  //       this.coursesService.remove(course._id).subscribe(
+  //         () => {
+  //           this.refresh();
+  //           this.snackBar.open('Curso removido com sucesso!', 'X', {
+  //             duration: 5000,
+  //             verticalPosition: 'top',
+  //             horizontalPosition: 'center'
+  //           });
+  //         },
+  //         () => this.onError('Erro ao tentar remover curso.')
+  //       );
+  //     }
+  //   });
   }
 }
